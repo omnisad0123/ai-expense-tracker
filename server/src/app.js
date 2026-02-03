db25e67a5c686744ef473400e571c800bcc390b9
om.prakashnisad.ece20@itbhu.ac.in <omnisad0123>
2026-02-03 19:49:03 +0530

    Login and register api down with jwt based authentication

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/health", (req, res) => {
   res.json({ status: "OK" });

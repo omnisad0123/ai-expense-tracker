b8644de404cb658c072da83edcaf5edd66dd4b77
om.prakashnisad.ece20@itbhu.ac.in <omnisad0123>
2026-02-25 22:50:26 +0530

    fix: updated baseURL to include /api prefix

--- client/AiExpensetracker/src/services/api.js
+++ client/AiExpensetracker/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ai-expense-tracker-p7jg.onrender.com",
+  baseURL: "https://ai-expense-tracker-p7jg.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
